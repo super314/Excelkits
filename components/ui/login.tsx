f3a3db6783e9a75bb4c6dfb68d9e45e91607a6db
Super <newest.star1990@gmail.com>
2024-12-23 01:34:56 -0500

    feat: added login, register

---
+++ components/ui/login.tsx
@@ -0,0 +1,64 @@
+import Link from "next/link";
+import { ReadonlyURLSearchParams, redirect } from "next/navigation";
+import { createClient } from "@/lib/supabase/ssr/server";
+
+export interface RegisterProps {
+  params: ReadonlyURLSearchParams;
+}
+
+const Login = ({ params }: RegisterProps) => {
+  const { email, password, error }: any = params;
+
+  const onLogin = async (formData: FormData) => {
+    "use server";
+
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    const supabase = createClient();
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      return redirect(`/auth/login?email=${email}&password=${password}&error=${error.message}`);
+    }
+
+    return redirect("/todo");
+  };
+
+  return (
+    <div>
+      <p className="mb-6 text-center text-xl">SignIn</p>
+      <form action={onLogin}>
+        {error && (
+          <div className="bg-red-200 mb-4 px-4 py-2 border border-red-300 rounded-md text-gray-700 text-sm leading-tight">
+            {error}
+          </div>
+        )}
+        <input
+          name="email"
+          className="mb-4 px-4 py-1 border rounded-md w-full"
+          defaultValue={email}
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          name="password"
+          className="mb-4 px-4 py-1 border rounded-md w-full"
+          defaultValue={password}
+          placeholder="Password"
+        />
+        <p className="mb-4">
+          <Link href="/auth/forgot" className="text-blue-400 text-sm">
+            Forgot Password?
+          </Link>
+        </p>
+        <input type="submit" className="block hover:bg-gray-50 py-1 border rounded-md w-full cursor-pointer" />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
